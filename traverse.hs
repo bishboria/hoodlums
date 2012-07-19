2aa5b51931173007073763ea1a49c9d5d75af38e
Stuart Gale <bishboria@gmail.com>
2012-07-19 19:28:16 +0100

    update 2

--- traverse.hs
+++ traverse.hs
@@ -1,11 +1,20 @@
+{-# language TypeSynonymInstances, FlexibleInstances #-}
+
 module Traverse where
 
-data Tree a = Node a [Tree a] deriving Show
+data Tree a = Node a [Tree a]
+            deriving Show
 
-pretty :: Show a => Tree a -> String
-pretty tree = pprint 0 tree
-    where
-        pprint n (Node name ns) =
-            replicate (n*2) ' ' ++ show name ++ "\n" ++ concatMap (pprint (n+1)) ns
+class Pretty a where
+    pretty :: a -> String
+
+instance Pretty String where
+    pretty s = s
+
+instance (Pretty a) => Pretty (Tree a) where
+    pretty tree = pprint 0 tree
+        where
+            pprint n (Node name ns) =
+                replicate (n*2) ' ' ++ pretty name ++ "\n" ++ concatMap (pprint (n+1)) ns
 
 testTree = Node "root" [(Node "one" []), (Node "two" [(Node "three" [])])]
