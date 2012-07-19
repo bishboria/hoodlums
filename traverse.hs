6b7af350d44079ea2ee2c1fecb433ebcc0d5d06a
Stuart Gale <bishboria@gmail.com>
2012-07-19 20:11:16 +0100

    need to find Data.Generics.Aliases package..

--- traverse.hs
+++ traverse.hs
@@ -1,9 +1,13 @@
-{-# language TypeSynonymInstances, FlexibleInstances #-}
+{-# language TypeSynonymInstances, FlexibleInstances, DeriveDataTypeable #-}
 
 module Traverse where
 
+import Data.Data
+import Data.Typeable
+import Data.Generics.Aliases
+
 data Tree a = Node a [Tree a]
-            deriving Show
+            deriving (Show, Data, Typeable)
 
 class Pretty a where
     pretty :: a -> String
@@ -18,3 +22,20 @@ instance (Pretty a) => Pretty (Tree a) where
                 replicate (n*2) ' ' ++ pretty name ++ "\n" ++ concatMap (pprint (n+1)) ns
 
 testTree = Node "root" [(Node "one" []), (Node "two" [(Node "three" [])])]
+
+
+-- copied from Hoogle's gshow and then modified to name as we need...
+gpretty :: Data a => a -> String
+gpretty x = gprettys x ""
+
+-- | Generic shows
+gprettys :: Data a => a -> ShowS
+
+-- -- This is a prefix-show using surrounding "(" and ")",
+-- -- where we recurse into subterms with gmapQ.
+gprettys = ( \t ->
+               showChar '('
+             . (showString . showConstr . toConstr $ t)
+             . (foldr (.) id . gmapQ ((showChar ' ' .) . gprettys) $ t)
+             . showChar ')'
+           ) `extQ` (shows :: String -> ShowS)
