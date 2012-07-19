23e834a35f05f7bfed857749b84859e351024876
Stuart Gale <bishboria@gmail.com>
2012-07-19 19:51:31 +0100

    They decided to roll back...

--- traverse.hs
+++ traverse.hs
@@ -1,27 +1,20 @@
 {-# language TypeSynonymInstances, FlexibleInstances #-}
 
--- Trying to create an unfixed structure so you can pretty print
--- some other (recursive) data structure
-
 module Traverse where
 
-data TreeF a r = Node a [r]
+data Tree a = Node a [Tree a]
             deriving Show
 
-newtype Fix f = Fix (f (Fix f))
+class Pretty a where
+    pretty :: a -> String
 
-type Tree a = Fix (TreeF a)
+instance Pretty String where
+    pretty s = s
 
-{-class Pretty a where-}
-    {-pretty :: a -> String-}
+instance (Pretty a) => Pretty (Tree a) where
+    pretty tree = pprint 0 tree
+        where
+            pprint n (Node name ns) =
+                replicate (n*2) ' ' ++ pretty name ++ "\n" ++ concatMap (pprint (n+1)) ns
 
-{-instance Pretty String where-}
-    {-pretty s = s-}
-
-{-instance (Pretty a) => Pretty (Tree a) where-}
-    {-pretty tree = pprint 0 tree-}
-        {-where-}
-            {-pprint n (Node name ns) =-}
-                {-replicate (n*2) ' ' ++ pretty name ++ "\n" ++ concatMap (pprint (n+1)) ns-}
-
-{-testTree = Node "root" [(Node "one" []), (Node "two" [(Node "three" [])])]-}
+testTree = Node "root" [(Node "one" []), (Node "two" [(Node "three" [])])]
