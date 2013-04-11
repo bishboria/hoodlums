bc9f311ac3e77cd3d2eef1eef2f056ef903eef62
Stuart Gale <bishboria@gmail.com>
2013-04-11 20:38:17 +0100

    Use type class to overload handlers.

--- 201304/Main.hs
+++ 201304/Main.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Main where
 
 import Database.SQLite3
 import Data.Text
 import Data.Int (Int64)
+import Data.Function (fix)
 
 data Album = Album {
       albumIdx       :: Int64
@@ -12,34 +14,40 @@ data Album = Album {
     , albumArtistIdx :: Int64
     } deriving (Show)
 
+class HasHandler h where
+  getHandler :: h -> Statement -> ColumnIndex -> IO ()
+
+instance HasHandler (IO ()) where
+  getHandler h _ _ = h
+
+instance (HasHandler a) => HasHandler (Int64 -> a) where
+  getHandler f s i = do
+    v <- columnInt64 s i
+    getHandler (f v) s (i + 1)
+
+instance (HasHandler a) => HasHandler (Text -> a) where
+  getHandler f s i = do
+    v <- columnText s i
+    getHandler (f v) s (i + 1)
+
 dbname = "chinook.sqlite"
 query  = "select * from album;"
 
 main :: IO ()
 main = do
     db <- open dbname
-    statement <- prepare db query
-    untilM_ testDone $ processRow statement
+    execQuery db query processRow
 
-untilM_ :: (a -> Bool) -> IO a -> IO ()
-untilM_ p a = do
-    r <- a
-    if p r
-       then return ()
-       else untilM_ p a
+processRow :: Int64 -> Text -> Int64 -> IO ()
+processRow idx title artistIdx = print $ Album idx title artistIdx
 
-testDone :: StepResult -> Bool
-testDone Done = True
-testDone Row  = False
-
-processRow :: Statement -> IO StepResult
-processRow statement = do
-    stepResult <- step statement
-    case stepResult of
-        Row -> do
-            idx <- columnInt64 statement 0
-            title <- columnText statement 1
-            artistIdx <- columnInt64 statement 2
-            print $ Album idx title artistIdx
-        Done -> putStrLn "All done"
-    return stepResult
+execQuery :: (HasHandler a) => Database -> Text -> a -> IO ()
+execQuery db q rowProcessor = do
+    statement <- prepare db q
+    fix $ \loop -> do
+        stepResult <- step statement
+        case stepResult of
+          Row -> do
+              getHandler rowProcessor statement 0
+              loop
+          Done -> return ()
