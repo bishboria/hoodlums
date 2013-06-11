8a54ddd9482226ff422310bb0e96b4eae992822d
Stuart Gale <bishboria@gmail.com>
2013-06-11 19:41:53 +0100

    Client can now communicate with lots of bulbs.

--- 201306/LightbulbClient.hs
+++ 201306/LightbulbClient.hs
@@ -1,17 +1,22 @@
 module LightBulbClient where
 
+import Data.Foldable (forM_)
 import Network.Socket
 import Control.Monad (forever, void)
 
 port = 9876
 host = "127.0.0.1"
 
+bulbHosts :: [String]
+bulbHosts = ["141.228.108.156", "141.228.108.152"]
+
+main :: IO ()
 main = withSocketsDo $ do
     s <- socket AF_INET Datagram defaultProtocol
+    bulbs <- mapM (fmap (SockAddrInet port) . inet_addr) bulbHosts
     setSocketOption s Broadcast 1
-    hostAddr <- inet_addr host
     void $ forever $ do
         msg <- getLine
-        sendTo s msg (SockAddrInet port hostAddr)
+        forM_ bulbs $ sendTo s msg
     sClose s
-    return()
+    return ()
