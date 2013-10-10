fb30937a331c7682eb8479fabf8a53ba7e63ace0
Stuart Gale <bishboria@gmail.com>
2013-10-10 19:52:29 +0100

    Making eval a foldr function

--- 201310/CompData.hs
+++ 201310/CompData.hs
@@ -25,6 +25,7 @@ prog = [FD 100, RT 90, FD 100, RT 90, FD 100, RT 90, FD 100, RT 90]
 
 prog' = [Repeat 4 [FD 100, RT 90]]
 
+{-
 eval :: [Logo] -> SVG
 eval prog = Path $ go prog (Turtle (0, 0) 0)
   where
@@ -33,6 +34,16 @@ eval prog = Path $ go prog (Turtle (0, 0) 0)
                                       in  L pos' : go ls (Turtle pos' dir)
     go (RT d : ls) (Turtle pos dir) = go ls (Turtle pos (dir + d))
     go (Repeat n is : ls) t = go (concat (replicate n is) ++ ls) t
+-}
+
+eval :: [Logo] -> SVG
+eval prog = Path $ foldr go (const []) prog (Turtle (0,0) 0)
+  where
+    go :: Logo -> (Turtle -> [Step]) -> (Turtle -> [Step])
+    go (FD d)        r (Turtle pos dir) = let pos' = offset pos dir d
+                                          in  L pos' : r (Turtle pos' dir)
+    go (RT d)        r (Turtle pos dir) = r (Turtle pos (dir + d))
+    go (Repeat n is) r _                = error "You haven't eliminated the repeat"
 
 offset :: Pos -> Dir -> Distance -> Pos
 offset (x, y) dir d = ( x + d * cos (dir / 180 * pi)
