f9c31f12e918e2c3703637d7e50d2c82d081c98f
Stuart Gale <bishboria@gmail.com>
2013-10-13 15:52:24 +0100

    End of session version.

--- 201310/CompLogo.hs
+++ 201310/CompLogo.hs
@@ -1,51 +1,89 @@
-module CompData where
+{-# LANGUAGE DeriveFunctor, TemplateHaskell, TypeOperators, ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -w #-}
+module CompLogo where
+
+import Data.Comp
+import Data.Comp.Derive
+import Prelude hiding (repeat)
 
 type Distance = Double
-type Pos      = (Double, Double)
-type Dir      = Double
 
+{-
+data Logo = FD Distance | RT Dir | Repeat Int [Logo]
+  deriving (Eq, Show)
+-}
 
-data Logo = FD Distance
-          | RT Dir
-          | Repeat Int [Logo]
-    deriving (Show, Eq)
+type Pos = (Double, Double)
+type Dir = Double
+
+data PrimLogoF r = FD Distance | RT Dir
+  deriving (Functor, Eq, Show)
+
+data SugarLogoF r = Repeat Int [r]
+  deriving (Functor, Eq, Show)
+
+type LogoF = PrimLogoF :+: SugarLogoF
+
+-- data (f :+: g) r = Inl (f r) | Inr (g r)
+
+-- data Term f = Term (f (Term f))
+
+derive [smartConstructors] [''PrimLogoF, ''SugarLogoF]
+
+type PrimLogo = Term PrimLogoF
+type Logo     = Term LogoF
 
 data SVG = Path [Step]
-    deriving (Show, Eq)
+  deriving (Eq, Show)
 
-data Step = M Pos
-          | L Pos
-    deriving (Show, Eq)
+data Step = M Pos | L Pos
+  deriving (Eq, Show)
 
 data Turtle = Turtle Pos Dir
-    deriving (Show, Eq)
+  deriving (Eq, Show)
 
 prog :: [Logo]
-prog = [FD 100, RT 90, FD 100, RT 90, FD 100, RT 90, FD 100, RT 90]
-
-prog' = [Repeat 4 [FD 100, RT 90]]
+prog  = [iFD 100, iRT 90, iFD 100, iRT 90, iFD 100, iRT 90, iFD 100, iRT 90]
+prog' :: [Logo]
+prog' = [iRepeat 4 [iFD 100, iRT 90]]
 
 {-
 eval :: [Logo] -> SVG
-eval prog = Path $ go prog (Turtle (0, 0) 0)
+eval prog = Path $ go prog (Turtle (0,0) 0)
   where
-    go [] _ = []
+    go []          _                = []
     go (FD d : ls) (Turtle pos dir) = let pos' = offset pos dir d
-                                      in  L pos' : go ls (Turtle pos' dir)
+                                      in L pos' : go ls (Turtle pos' dir)
     go (RT d : ls) (Turtle pos dir) = go ls (Turtle pos (dir + d))
-    go (Repeat n is : ls) t = go (concat (replicate n is) ++ ls) t
+    go (Repeat n is : ls) t         = go (concat (replicate n is) ++ ls)
+                                         t
 -}
 
-eval :: [Logo] -> SVG
-eval prog = Path $ foldr go (const []) prog (Turtle (0,0) 0)
+eval :: [PrimLogo] -> SVG
+eval pr = Path $ foldr go (const []) pr (Turtle (0,0) 0)
   where
-    go :: Logo -> (Turtle -> [Step]) -> (Turtle -> [Step])
-    go (FD d)        r (Turtle pos dir) = let pos' = offset pos dir d
-                                          in  L pos' : r (Turtle pos' dir)
-    go (RT d)        r (Turtle pos dir) = r (Turtle pos (dir + d))
-    go (Repeat n is) r _                = error "You haven't eliminated the repeat"
+    go :: PrimLogo -> (Turtle -> [Step]) -> Turtle -> [Step]
+    go (Term (FD d)) r (Turtle pos dir) = let pos' = offset pos dir d
+                                          in L pos' : r (Turtle pos' dir)
+    go (Term (RT d)) r (Turtle pos dir) = r (Turtle pos (dir + d))
 
 offset :: Pos -> Dir -> Distance -> Pos
 offset (x, y) dir d = ( x + d * cos (dir / 180 * pi)
                       , y + d * sin (dir / 180 * pi)
                       )
+
+unroll :: [Logo] -> [PrimLogo]
+unroll = foldr go []
+  where
+    go :: Logo -> [PrimLogo] -> [PrimLogo]
+    go (Term (proj -> Just (Repeat n is))) r =
+      concat (replicate n (unroll is)) ++ r
+    go (Term (proj -> Just (FD d))) r =
+      iFD d : r
+    go (Term (proj -> Just (RT d))) r =
+      iRT d : r
+    -- go (Term (proj -> Just (i :: PrimLogoF Logo))) r =
+    --   Term (inj i) : r
+    -- go i                                   r = i : r
+
