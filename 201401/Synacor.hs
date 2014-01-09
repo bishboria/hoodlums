ffd86aea104f88d364865966c7b13415fa1de32b
Stuart Gale <bishboria@gmail.com>
2014-01-09 20:16:20 +0000

    Using State monad

--- 201401/Synacor.hs
+++ 201401/Synacor.hs
@@ -2,21 +2,37 @@
 
 module Synacor where
 
+import           Control.Applicative
+import           Control.Monad.State.Strict
 import           Data.Char
 import qualified Data.IntMap.Strict as M
 import           Data.Word
 
-data State = State {
-                     mem   :: !(M.IntMap Word16)
-                   , stack :: [Word16]
-                   , ip    :: Int
-                   }
+data Machine = Machine
+  {
+    mem   :: !(M.IntMap Word16)
+  , stack :: [Word16]
+  , ip    :: Int
+  }
 
-run :: State -> String -> String
-run s@State{..} input = case mem M.! ip of
-                          0  -> []
-                          19 -> (chr $ fromIntegral $ mem M.! (ip+1)) : run s{ip = ip + 2} input
-                          21 -> run s{ip = ip + 1} input
+type Syn = State Machine
 
-load :: [Word16] -> State
-load prog = State (M.fromList $ zip [0..] prog) [] 0
+run :: Machine -> String -> String
+run m i = evalState (run' i) m
+
+run' :: String -> Syn String
+run' input = getMem 0 >>= \inst -> case inst of
+  0  -> return []
+  19 -> (:) <$>
+        (chr . fromIntegral <$> getMem 1) <*>
+        (incip 2 >> run' input)
+  21 -> incip 1 >> run' input
+
+incip :: Int -> Syn ()
+incip i = modify $ \s -> s{ip = ip s + i}
+
+getMem :: Int -> Syn Word16
+getMem offset = (M.!) <$> gets mem <*> gets ((+ offset) . ip)
+
+load :: [Word16] -> Machine
+load prog = Machine (M.fromList $ zip [0..] prog) [] 0
