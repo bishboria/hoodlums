76945ba2f7481f76980884c5cb7efedb43b3193e
Stuart Gale <bishboria@gmail.com>
2014-01-09 20:45:17 +0000

    in and out instructions. extra memory stuff

--- 201401/Synacor.hs
+++ 201401/Synacor.hs
@@ -23,16 +23,37 @@ run m i = evalState (run' i) m
 run' :: String -> Syn String
 run' input = getMem 0 >>= \inst -> case inst of
   0  -> return []
-  19 -> (:) <$>
-        (chr . fromIntegral <$> getMem 1) <*>
-        (incip 2 >> run' input)
+  19 -> do
+    c <- chr . fromIntegral <$> getValue 1
+    incip 2
+    (c:) <$> run' input
+  20 -> do
+    let (c:input') = input
+    addr <- getAddr 1
+    store addr (fromIntegral $ ord c)
+    incip 2
+    run' input'
   21 -> incip 1 >> run' input
 
 incip :: Int -> Syn ()
 incip i = modify $ \s -> s{ip = ip s + i}
 
+store :: Int -> Word16 -> Syn ()
+store addr value = modify $ \s -> s{mem = M.insert addr value (mem s)}
+
 getMem :: Int -> Syn Word16
 getMem offset = (M.!) <$> gets mem <*> gets ((+ offset) . ip)
+
+getAddr :: Int -> Syn Int
+getAddr offset = fromIntegral <$> getMem offset
+
+getValue :: Int -> Syn Word16
+getValue n = do
+    i <- getMem n
+    if i > 32767 then retrieve (fromIntegral i) else return i
+
+retrieve :: Int -> Syn Word16
+retrieve addr = (M.! addr) <$> gets mem
 
 load :: [Word16] -> Machine
 load prog = Machine (M.fromList $ zip [0..] prog) [] 0
